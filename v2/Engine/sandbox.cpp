7f5a31ee035781915d2722f9b6d7cb660e010c7d
Campbell Blackburn <campbell.blackburn@oni.gov.au>
2019-07-22 14:33:24 +1000

    Add sandbox UI example

--- v2/Engine/sandbox.cpp
+++ v2/Engine/sandbox.cpp
@@ -1,6 +1,8 @@
 #include "engine.hpp"
 #include <iostream>
 
+#include "imgui.h"
+
 #include "Engine/application.hpp"
 #include "Engine/layer.hpp"
 
@@ -8,6 +10,18 @@ class MyLayer: public engine::Layer {
 
     void on_attach() override {
         GAME_INFO("My layer attached");
+    }
+
+    void on_ui_render() override {
+        static int counter = 0;
+        ImGui::Begin("Test");
+        ImGui::Text("Sandbox UI");
+        if (ImGui::Button("Count")){
+            counter++;
+        }
+        ImGui::SameLine();
+        ImGui::Text("Count: %i", counter);
+        ImGui::End();
     }
 };
 
