5432318f1426f3c9c0f5c71881909ecb6df8de96
Campbell <campbellblackburn@hotmail.com>
2019-09-02 22:51:08 +1000

    PBR Shader write
    Former-commit-id: 756e95a451a9e878648ce8a255eaf59e40c4846e

--- v2/shaders/fragment.glsl
+++ v2/shaders/fragment.glsl
@@ -1,24 +1,116 @@
 #version 460
 
 in vec3 f_normal;
-in vec4 f_fragpos;
+in vec3 f_worldpos;
 in vec2 f_texcoord;
 
-uniform sampler2D base_color;
+uniform sampler2D albedo;
+uniform sampler2D normal;
+// Roughness Metallic Ambient Occlusion map
+// Ambient Occlusion from R channel
+// Roughness from G channel
+// Metallic from B channel
+uniform sampler2D ambient_roughness_metallic;
+
 uniform vec3 u_lightpos = vec3(2, 1, 2);
 uniform vec3 u_lightcolor = vec3(1, 1, 1);
+uniform vec4 u_color;
 uniform float ambient_strength = 0.1;
+
+uniform vec3 u_camera_position = vec3(2, 1, 2);
 
 out vec4 out_color;
 
+const float PI = 3.14159265359;
+
+vec3 get_normal_from_map() {
+    vec3 tangent_normal = texture(normal, f_texcoord).xyz * 2.0 - 1.0;
+
+    vec3 Q1 = dFdx(f_worldpos);
+    vec3 Q2 = dFdy(f_worldpos);
+    vec3 st1 = dFdx(f_worldpos);
+    vec3 st2 = dFdy(f_worldpos);
+
+    vec3 N = normalize(f_normal);
+    vec3 T = normalize(Q1 * st2.t - Q2 * st1.t);
+    vec3 B = -normalize(cross(N, T));
+    mat3 TBN = mat3(T, B, N);
+
+    return normalize(TBN * tangent_normal);
+}
+
+float distribution_ggx(vec3 N, vec3 H, float roughness) {
+    return 0.0;
+}
+
+float geometry_schlick_ggx(float NdotV, float roughness) {
+    return 0.0;
+}
+
+float geometry_smith(vec3 N, vec3 V, vec3 L, float roughness) {
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2 = geometry_schlick_ggx(NdotV, roughness);
+    float ggx1 = geometry_schlick_ggx(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}
+
+vec3 fresnel_schlick(float cos_theta, vec3 F0)
+{
+    return F0 + (1.0 - F0) * pow(1.0 - cos_theta, 5.0);
+}
+
 void main() {
-    vec3 ambient = ambient_strength * u_lightcolor;
-    vec3 norm = normalize(f_normal);
-    vec3 lightdir = normalize(u_lightpos - f_fragpos.xyz);  
-    float diff = max(dot(norm, lightdir), 0.0);
-    vec3 diffuse = diff * u_lightcolor;
 
-    vec3 result = (ambient + diffuse) * texture(base_color, f_texcoord).xyz;
+    //vec3 albedo_sample = pow(texture2D(albedo, f_texcoord).rgb, vec3(2.2)); // sRGB Converter
+    vec3 albedo_sample = texture(albedo, f_texcoord).rgb;
+    float ambient_occlusion = texture(ambient_roughness_metallic, f_texcoord).r;
+    float roughness = texture(ambient_roughness_metallic, f_texcoord).g;
+    float metallic = texture(ambient_roughness_metallic, f_texcoord).b;
 
-    out_color = vec4(result, 1);
+    vec3 N = get_normal_from_map();
+    vec3 V = normalize(u_camera_position - f_worldpos);
+
+    vec3 F0 = vec3(0.4);
+    F0 = mix(F0, albedo_sample, metallic);
+
+    // reflectance equation
+    vec3 Lo = vec3(0.0);
+    {  // Calculate per light
+        vec3 L = normalize(u_lightpos - f_worldpos);
+        vec3 H = normalize(V + L);
+
+        float distance = length(u_lightpos - f_worldpos);
+        float attenuation = 1.0 / (distance * distance);
+        vec3 radiance = u_lightcolor * attenuation;
+
+        //Cook-Torrance BRDF
+        float NDF = distribution_ggx(N, H, roughness);
+        float G = geometry_smith(N, V, L, roughness);
+        vec3 F = fresnel_schlick(max(dot(H, V), 0.0), F0);
+        vec3 nominator = NDF * G * F;
+        float denominator = 4 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.001;  // To prevent divide by zero
+        vec3 specular = nominator / denominator;
+
+        // kS is equal to fresnel
+        vec3 kS = F;
+        vec3 kD = vec3(1.0) - kS;
+
+        kD *= 1.0 - metallic;
+
+        float NdotL = max(dot(N, L), 0.0);
+
+        Lo += (kD * albedo_sample / PI + specular) * radiance * NdotL;
+    }
+
+    vec3 ambient = vec3(0.03) * albedo_sample * ambient_occlusion;
+
+    vec3 color = ambient * Lo;
+    // HDR tonemapping
+    color = color / (color + vec3(1.0));
+    // Gamma correction
+    color = pow(color, vec3(1.0/2.2));
+
+    out_color = vec4(color, 1);
 }
--- v2/shaders/vertex.glsl
+++ v2/shaders/vertex.glsl
@@ -8,12 +8,12 @@ uniform mat4 u_view_projection;
 uniform mat4 u_model;
 
 out vec3 f_normal;
-out vec4 f_fragpos;
+out vec3 f_worldpos;
 out vec2 f_texcoord;
 
 void main() {
     f_texcoord = texcoord;
     f_normal = normal.xyz;
-    f_fragpos = u_model * position;
+    f_worldpos = (u_model * position).xyz;
     gl_Position = u_view_projection * u_model * position;
 }
