de96cff4c3e0a4c18b363a8d2c9c7eabd3d65f85
Campbell <campbellblackburn@hotmail.com>
2019-11-15 18:35:14 +1100

    Remove old project

--- include/identifier.hpp
+++
@@ -1,7 +0,0 @@
-/*
-Unique ID manager
-
-This is only unique for the lifetime of the program
-*/
-
-long get_uid();
--- src/identifier.cpp
+++
@@ -1,8 +0,0 @@
-#include "identifier.hpp"
-
-long current_id = 10000;
-
-long get_uid() {
-    current_id++;
-    return current_id;
-}
