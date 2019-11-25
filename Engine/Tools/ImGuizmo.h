3354af480e45cd538ea3eea5c80bb31061f243cb
Campbell Blackburn <campbell.blackburn@oni.gov.au>
2019-11-26 10:14:52 +1100

    Fix whitespace in imported file

--- Engine/Tools/ImGuizmo.h
+++ Engine/Tools/ImGuizmo.h
@@ -58,64 +58,64 @@
 
 namespace ImGuizmo
 {
-	// call inside your own window and before Manipulate() in order to draw gizmo to that window.
-	IMGUI_API void SetDrawlist();
+    // call inside your own window and before Manipulate() in order to draw gizmo to that window.
+    IMGUI_API void SetDrawlist();
 
-	// call BeginFrame right after ImGui_XXXX_NewFrame();
-	IMGUI_API void BeginFrame();
+    // call BeginFrame right after ImGui_XXXX_NewFrame();
+    IMGUI_API void BeginFrame();
 
-	// return true if mouse cursor is over any gizmo control (axis, plan or screen component)
-	IMGUI_API bool IsOver();
+    // return true if mouse cursor is over any gizmo control (axis, plan or screen component)
+    IMGUI_API bool IsOver();
 
-	// return true if mouse IsOver or if the gizmo is in moving state
-	IMGUI_API bool IsUsing();
+    // return true if mouse IsOver or if the gizmo is in moving state
+    IMGUI_API bool IsUsing();
 
-	// enable/disable the gizmo. Stay in the state until next call to Enable.
-	// gizmo is rendered with gray half transparent color when disabled
-	IMGUI_API void Enable(bool enable);
+    // enable/disable the gizmo. Stay in the state until next call to Enable.
+    // gizmo is rendered with gray half transparent color when disabled
+    IMGUI_API void Enable(bool enable);
 
-	// helper functions for manualy editing translation/rotation/scale with an input float
-	// translation, rotation and scale float points to 3 floats each
-	// Angles are in degrees (more suitable for human editing)
-	// example:
-	// float matrixTranslation[3], matrixRotation[3], matrixScale[3];
-	// ImGuizmo::DecomposeMatrixToComponents(gizmoMatrix.m16, matrixTranslation, matrixRotation, matrixScale);
-	// ImGui::InputFloat3("Tr", matrixTranslation, 3);
-	// ImGui::InputFloat3("Rt", matrixRotation, 3);
-	// ImGui::InputFloat3("Sc", matrixScale, 3);
-	// ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, gizmoMatrix.m16);
-	//
-	// These functions have some numerical stability issues for now. Use with caution.
-	IMGUI_API void DecomposeMatrixToComponents(const float *matrix, float *translation, float *rotation, float *scale);
-	IMGUI_API void RecomposeMatrixFromComponents(const float *translation, const float *rotation, const float *scale, float *matrix);
+    // helper functions for manualy editing translation/rotation/scale with an input float
+    // translation, rotation and scale float points to 3 floats each
+    // Angles are in degrees (more suitable for human editing)
+    // example:
+    // float matrixTranslation[3], matrixRotation[3], matrixScale[3];
+    // ImGuizmo::DecomposeMatrixToComponents(gizmoMatrix.m16, matrixTranslation, matrixRotation, matrixScale);
+    // ImGui::InputFloat3("Tr", matrixTranslation, 3);
+    // ImGui::InputFloat3("Rt", matrixRotation, 3);
+    // ImGui::InputFloat3("Sc", matrixScale, 3);
+    // ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, gizmoMatrix.m16);
+    //
+    // These functions have some numerical stability issues for now. Use with caution.
+    IMGUI_API void DecomposeMatrixToComponents(const float *matrix, float *translation, float *rotation, float *scale);
+    IMGUI_API void RecomposeMatrixFromComponents(const float *translation, const float *rotation, const float *scale, float *matrix);
 
-	IMGUI_API void SetRect(float x, float y, float width, float height);
-	// default is false
-	IMGUI_API void SetOrthographic(bool isOrthographic);
+    IMGUI_API void SetRect(float x, float y, float width, float height);
+    // default is false
+    IMGUI_API void SetOrthographic(bool isOrthographic);
 
-	// Render a cube with face color corresponding to face normal. Usefull for debug/tests
-	IMGUI_API void DrawCube(const float *view, const float *projection, const float *matrix);
-	IMGUI_API void DrawGrid(const float *view, const float *projection, const float *matrix, const float gridSize);
+    // Render a cube with face color corresponding to face normal. Usefull for debug/tests
+    IMGUI_API void DrawCube(const float *view, const float *projection, const float *matrix);
+    IMGUI_API void DrawGrid(const float *view, const float *projection, const float *matrix, const float gridSize);
 
-	// call it when you want a gizmo
-	// Needs view and projection matrices. 
-	// matrix parameter is the source matrix (where will be gizmo be drawn) and might be transformed by the function. Return deltaMatrix is optional
-	// translation is applied in world space
-	enum OPERATION
-	{
-		TRANSLATE,
-		ROTATE,
-		SCALE,
-		BOUNDS,
-	};
+    // call it when you want a gizmo
+    // Needs view and projection matrices. 
+    // matrix parameter is the source matrix (where will be gizmo be drawn) and might be transformed by the function. Return deltaMatrix is optional
+    // translation is applied in world space
+    enum OPERATION
+    {
+        TRANSLATE,
+        ROTATE,
+        SCALE,
+        BOUNDS,
+    };
 
-	enum MODE
-	{
-		LOCAL,
-		WORLD
-	};
+    enum MODE
+    {
+        LOCAL,
+        WORLD
+    };
 
-	IMGUI_API void Manipulate(const float *view, const float *projection, OPERATION operation, MODE mode, float *matrix, float *deltaMatrix = 0, float *snap = 0, float *localBounds = NULL, float *boundsSnap = NULL);
+    IMGUI_API void Manipulate(const float *view, const float *projection, OPERATION operation, MODE mode, float *matrix, float *deltaMatrix = 0, float *snap = 0, float *localBounds = NULL, float *boundsSnap = NULL);
    //
    // Please note that this cubeview is patented by Autodesk : https://patents.google.com/patent/US7782319B2/en
    // It seems to be a defensive patent in the US. I don't think it will bring troubles using it as
